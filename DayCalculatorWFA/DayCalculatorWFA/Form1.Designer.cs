13133e9c8ffa50ebcd858a7726ab5ec9aa61a65d
Lidor-sh <lidorschool3@gmail.com>
2017-07-20 04:44:58 +0300

    First Commit, initial project

---
+++ DayCalculatorWFA/DayCalculatorWFA/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace DayCalculatorWFA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Day = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Month = new System.Windows.Forms.TextBox();
+            this.Year = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Result = new System.Windows.Forms.Label();
+            this.Check = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Day
+            // 
+            this.Day.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.Day.Location = new System.Drawing.Point(189, 52);
+            this.Day.MaxLength = 2;
+            this.Day.Name = "Day";
+            this.Day.Size = new System.Drawing.Size(70, 38);
+            this.Day.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label1.Location = new System.Drawing.Point(229, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 76);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "/";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label2.Location = new System.Drawing.Point(184, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Day";
+            // 
+            // Month
+            // 
+            this.Month.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.Month.Location = new System.Drawing.Point(277, 52);
+            this.Month.MaxLength = 2;
+            this.Month.Name = "Month";
+            this.Month.Size = new System.Drawing.Size(70, 38);
+            this.Month.TabIndex = 3;
+            // 
+            // Year
+            // 
+            this.Year.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.Year.Location = new System.Drawing.Point(376, 52);
+            this.Year.MaxLength = 4;
+            this.Year.Name = "Year";
+            this.Year.Size = new System.Drawing.Size(70, 38);
+            this.Year.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label3.Location = new System.Drawing.Point(318, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 76);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "/";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label4.Location = new System.Drawing.Point(272, 24);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 25);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Month";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label5.Location = new System.Drawing.Point(376, 24);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 25);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Year";
+            // 
+            // Result
+            // 
+            this.Result.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Result.AutoSize = true;
+            this.Result.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.Result.Location = new System.Drawing.Point(252, 121);
+            this.Result.Name = "Result";
+            this.Result.Size = new System.Drawing.Size(126, 46);
+            this.Result.TabIndex = 8;
+            this.Result.Text = "label6";
+            this.Result.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // Check
+            // 
+            this.Check.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Check.Location = new System.Drawing.Point(260, 181);
+            this.Check.Name = "Check";
+            this.Check.Size = new System.Drawing.Size(110, 46);
+            this.Check.TabIndex = 9;
+            this.Check.Text = "Check";
+            this.Check.UseVisualStyleBackColor = true;
+            this.Check.Click += new System.EventHandler(this.Check_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(630, 261);
+            this.Controls.Add(this.Check);
+            this.Controls.Add(this.Result);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Year);
+            this.Controls.Add(this.Month);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Day);
+            this.Name = "Form1";
+            this.Text = "Day Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Day;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Month;
+        private System.Windows.Forms.TextBox Year;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label Result;
+        private System.Windows.Forms.Button Check;
+    }
+}
+
---
+++ DayCalculatorWFA/DayCalculatorWFA/Form1.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DayCalculatorWFA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Day Settings
+            this.Day.AutoSize = false;
+            this.Day.Height = 40;
+            this.Day.Width = 40;
+            //Month Settings
+            this.Month.AutoSize = false;
+            this.Month.Height = 40;
+            this.Month.Width = 40;
+            //Year Settings
+            this.Year.AutoSize = false;
+            this.Year.Height = 40;
+            this.Year.Width = 70;
+            //Result Settings(Label)
+            this.Result.Text = "";
+            Result.Left = 200;
+        }
+
+        public static int YearCode(int year)
+        {
+            int yearcode;
+            yearcode = (year % 100 + (year % 100) / 4) % 7;
+            return yearcode;
+        }
+
+        public static int MonthCode(int month)
+        {
+            int monthcode = -1;
+            switch (month)
+            {
+                case 1:
+                case 10:
+                    monthcode = 0;
+                    break;
+                case 2:
+                case 3:
+                case 11:
+                    monthcode = 3;
+                    break;
+                case 4:
+                case 7:
+                    monthcode = 6;
+                    break;
+                case 5:
+                    monthcode = 1;
+                    break;
+                case 6:
+                    monthcode = 4;
+                    break;
+                case 8:
+                    monthcode = 2;
+                    break;
+                case 9:
+                case 12:
+                    monthcode = 5;
+                    break;
+            }
+            return monthcode;
+        }
+
+        public static int CenturyCode(int year)
+        {
+            int centurycode = -1;
+            if (year < 1799)
+                centurycode = 4;
+            else if (year < 1899)
+                centurycode = 2;
+            else if (year < 1999)
+                centurycode = 0;
+            else if (year < 2099)
+                centurycode = 6;
+            else if (year < 2199)
+                centurycode = 4;
+            else if (year < 2299)
+                centurycode = 2;
+            else
+                centurycode = 0;
+            return centurycode;
+        }
+
+        public static int LeapYearCode(int year, int month)
+        {
+            int leapyearcode = 0;
+            if (year % 4 == 0)
+            {
+                if (month <= 2)
+                    leapyearcode = 1;
+            }
+            return leapyearcode;
+        }
+
+        public static bool CheckDay(int day,int year,int month)
+        {
+            switch (month)
+            {
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                case 12:
+                    if (day <= 31 && day > 0)
+                        return true;
+                    break;
+                case 2:
+                    if (year % 4 == 0)
+                    {
+                        if (day <= 29 && day > 0)
+                            return true;
+                    }
+                    else
+                    {
+                        if (day <= 28 && day > 0)
+                            return true;
+                    }
+                    break;
+                case 4:
+                case 6:
+                case 9:
+                case 11:
+                    if (day <= 30 && day > 0)
+                        return true;
+                    break;
+            }
+            return false;
+        }
+
+        private void Check_Click(object sender, EventArgs e)
+        {
+            int year, month, day;
+            bool GoodDay = false;
+            TextBox daytb = (TextBox)Day;
+            TextBox monthtb = (TextBox)Month;
+            TextBox yeartb = (TextBox)Year;
+            int parsedValue;
+            if(daytb.TextLength == 0 || monthtb.TextLength == 0 || yeartb.TextLength == 0)
+            {
+                Result.Text = "Fill the fields";
+                Result.Font = new Font(Result.Font.Name, 30, Result.Font.Style, Result.Font.Unit);
+            }
+            else if (int.TryParse(Day.Text, out parsedValue) && int.TryParse(Month.Text, out parsedValue) && int.TryParse(Year.Text, out parsedValue))
+            {
+                day = Convert.ToInt32(daytb.Text);
+                month = Convert.ToInt32(monthtb.Text);
+                year = Convert.ToInt32(yeartb.Text);
+                if (year >= 2400 || year < 1700)
+                {
+                    Result.Text = "Choose year between 1700-2399";
+                    Result.Font = new Font(Result.Font.Name, 15, Result.Font.Style, Result.Font.Unit);
+                }
+                else if (month > 12 || month < 1)
+                {
+                    Result.Text = "Choose month between 1-12";
+                    Result.Font = new Font(Result.Font.Name, 15, Result.Font.Style, Result.Font.Unit);
+                }
+                else if (!CheckDay(day, year, month))
+                {
+                    Result.Text = "Choose a correct day";
+                    Result.Font = new Font(Result.Font.Name, 15, Result.Font.Style, Result.Font.Unit);
+                }
+                else
+                {
+                    int TheDayNumber;
+                    TheDayNumber = (YearCode(year) + MonthCode(month) + CenturyCode(year) + day - LeapYearCode(year, month)) % 7;
+                    switch (TheDayNumber)
+                    {
+                        case 0:
+                            Result.Text = "Day:Sunday";
+                            Result.Font = new Font(Result.Font.Name, 30, Result.Font.Style, Result.Font.Unit);
+                            break;
+                        case 1:
+                            Result.Text = "Day:Monday";
+                            Result.Font = new Font(Result.Font.Name, 30, Result.Font.Style, Result.Font.Unit);
+                            break;
+                        case 2:
+                            Result.Text = "Day:Tuesday";
+                            Result.Font = new Font(Result.Font.Name, 30, Result.Font.Style, Result.Font.Unit);
+                            break;
+                        case 3:
+                            Result.Text = "Day:Wednesday";
+                            Result.Font = new Font(Result.Font.Name, 30, Result.Font.Style, Result.Font.Unit);
+                            break;
+                        case 4:
+                            Result.Text = "Day:thursday";
+                            Result.Font = new Font(Result.Font.Name, 30, Result.Font.Style, Result.Font.Unit);
+                            break;
+                        case 5:
+                            Result.Text = "Day:Friday";
+                            Result.Font = new Font(Result.Font.Name, 30, Result.Font.Style, Result.Font.Unit);
+                            break;
+                        case 6:
+                            Result.Text = "Day:Saturday";
+                            Result.Font = new Font(Result.Font.Name, 30, Result.Font.Style, Result.Font.Unit);
+                            break;
+                    }
+                }
+
+            }
+            else
+            {
+                Result.Text = "Only numbers";
+                Result.Font = new Font(Result.Font.Name, 30, Result.Font.Style, Result.Font.Unit);
+            }
+            
+        }
+    }
+}
